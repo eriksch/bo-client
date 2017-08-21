43f6e7893c54d20d3529aade19c64ef3e05435c8
Erik Scholten <erik.scholten@trimm.nl>
2017-08-21 13:16:33 +0200

    Updated on API and conversation

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 const host = window.document.location.host.replace(/:.*/, '');
 const accessToken = '5199210cf8e04a95813e66efe9c59693';
+const client = new ApiAi.ApiAiClient({accessToken: accessToken});
 const baseUrl = 'https://api.api.ai/v1/';
 const messageSorry = "I'm sorry, I don't have the answer to that yet.";
 const session = Math.random();
@@ -14,13 +15,24 @@ ws.onclose = () => showDebug('WebSocket connection closed');
 ws.onmessage = (event) => handleMessage(JSON.parse(event.data));
 
 $(window).on('keydown', function(event) {
-  let msg = 'can you recommend an Action movie?';
+  let msg = '';
   switch (event.keyCode) {
     case 50:
-      addQuestion(msg)
-      send(msg);
+        msg = 'can you recommend me an Action movie?';
       break;
+    case 51:
+      msg = 'can you show me the poster?';
+    break;
+    case 52:
+      msg = 'whats it about?';
+      break;
+    default:
+      return;
   }
+
+  addQuestion(msg)
+  send(msg);
+
 });
 
 function showDebug(msg) {
@@ -49,47 +61,56 @@ function showMessage(msg) {
 }
 
 function send(msg) {
-
-    $.ajax({
-      type: "POST",
-      url: baseUrl + "query",
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      headers: {
-        "Authorization": "Bearer " + accessToken
-      },
-      data: JSON.stringify({query: msg, lang: "en", sessionId: "bo-client-" + session }),
-
-      success: function(data) {
-        prepareResponse(data);
-      },
-      error: function() {
-        respond("Connection could not be established");
-      }
+  client.textRequest(msg)
+    .then(function(data) {
+      prepareResponse(data);
+    })
+    .catch(function() {
+      respond("Connection could not be established");
     });
+}
+
+function prepareResponse(data) {
+  var debugJSON = JSON.stringify(data, undefined, 2),
+    spokenResponse = data.result.speech;
+
+  if (data.result.fulfillment.messages) {
+    data.result.fulfillment.messages.forEach(function(message) {
+      if (message.type === 4) {
+        respondWithImage(message.payload.poster_path);
+        return;
+      }
+    })
   }
 
-  function prepareResponse(val) {
-    var debugJSON = JSON.stringify(val, undefined, 2),
-      spokenResponse = val.result.speech;
-
+  if (data.result.fulfillment) {
+    spokenResponse = data.result.fulfillment.speech;
     respond(spokenResponse);
   }
 
-  function respond(val) {
-    if (val == "") {
-      val = messageSorry;
-    }
 
-    $('#messages').prepend('<cf-chat-response class="robot peak-thumb show animated fadeIn"><thumb></thumb><text><p class="show">' + val +'</p></text></cf-chat-response>');
+}
 
-    var msg = new SpeechSynthesisUtterance();
-    msg.voiceURI = "native";
-    msg.text = val;
-    msg.lang = "en-US";
-    window.speechSynthesis.speak(msg);
+function respondWithImage(path) {
+  $('#messages').prepend('<cf-chat-response class="robot peak-thumb show animated fadeIn"><thumb></thumb><p class="show"><img src="http://image.tmdb.org/t/p/w154/' + path +'" /></p></cf-chat-response>');
+}
+
+function respond(val) {
+  if (val == "") {
+    val = messageSorry;
   }
 
-  function addQuestion(msg) {
-    $('#messages').prepend('<cf-chat-response class="user show peak-thumb animated fadeIn"><thumb></thumb><text><p class="show">' + msg + '</p></text></cf-chat-response>');
-  }
+  //if (indexOf('http://') )
+
+  $('#messages').prepend('<cf-chat-response class="robot peak-thumb show animated fadeIn"><thumb></thumb><text><p class="show">' + val +'</p></text></cf-chat-response>');
+
+  var msg = new SpeechSynthesisUtterance();
+  msg.voiceURI = "native";
+  msg.text = val;
+  msg.lang = "en-US";
+  window.speechSynthesis.speak(msg);
+}
+
+function addQuestion(msg) {
+  $('#messages').prepend('<cf-chat-response class="user show peak-thumb animated fadeIn"><thumb></thumb><text><p class="show">' + msg + '</p></text></cf-chat-response>');
+}
